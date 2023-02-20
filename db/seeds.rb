1b2fd7841175fdbe748099c03bd69c8f9aacf7e7
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-02-20 20:11:13 +0100

    feat: make user class

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+User.destroy_all
+
+def create_users
+  User.create(email: 'mail@mail.com', password: '123123')
+  User.create(email: 'mail@test.com', password: '123123')
+
+  puts "Created #{User.all.count} users!"
+end
+
+create_users
