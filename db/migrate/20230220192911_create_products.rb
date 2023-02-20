6611ca6e58519fed8ef40cf5ef3f412dfbe75c5b
Ryuichiro Suzuki (Scooter) <46629248+SuzukiRyuichiro@users.noreply.github.com>
2023-02-20 21:22:41 +0100

    feat/generate product (#4)
    * feat: config rubocop so so many changes
    
    * feat: make products table
    
    * feat: seed product with fake name nad images

---
+++ db/migrate/20230220192911_create_products.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
